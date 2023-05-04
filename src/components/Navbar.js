151cd92de6f2b25eef70ffb7489dc3d0307f00a6
upendrari02 <upendrari02@gmail.com>
2023-05-05 02:19:19 +0530

    2nd day code

---
+++ src/components/Navbar.js
@@ -0,0 +1,57 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+export default function Navbar(props) {
+  return (
+    <nav
+      className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}
+    >
+      <div className="container-fluid">
+        <a className="navbar-brand" href="/">
+          {props.title}
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <div
+            className={`form-check form-switch text-${
+              props.mode === "light" ? "dark" : "light"
+            }`}
+          >
+            <input
+              className="form-check-input"
+              onClick={props.toggleMode}
+              type="checkbox"
+              id="flexSwitchCheckDefault"
+            />
+            <label
+              className="form-check-label"
+              htmlFor="flexSwitchCheckDefault"
+            >
+              Enable Dark Mode
+            </label>
+          </div>
+        </div>
+        <p>Created By- Upendra Rai</p>
+      </div>
+    </nav>
+  );
+}
+Navbar.prototype = {
+  title: PropTypes.string.isRequired,
+  aboutText: PropTypes.string.isRequired,
+};
+
+Navbar.defaultProps = {
+  title: "set Title",
+  aboutText: "set About Text",
+};
---
+++ src/components/TextForm.js
@@ -0,0 +1,102 @@
+import React, { useState } from "react";
+
+export default function TextForm(props) {
+  const handleUpClick = () => {
+    let newText = text.toUpperCase();
+    setText(newText);
+    props.showAlert(" Converted to UpperCase!","success");
+  };
+
+  const handleLoClick = () => {
+    let newText = text.toLowerCase();
+    setText(newText);
+    props.showAlert(" Converted to LowerCase!","success");
+
+  };
+
+  const handleClearClick = () => {
+    let newText = "";
+    setText(newText);
+    props.showAlert(" Text Cleared!","success");
+
+  };
+
+  const handleCopyClick = () => {
+    var texts = document.getElementById("textBox");
+    texts.select();
+    texts.setSelectionRange(0, 9999);
+    navigator.clipboard.writeText(texts.value);
+    props.showAlert(" Text Copied!","success");
+
+  };
+
+  const handleExtraSpacesClick = () => {
+    let newText = text.split(/[ ]+/);
+    setText(newText.join(" "));
+    props.showAlert(" Extra Spaces Removed!","success");
+  };
+
+  const handleOnChange = (event) => {
+    setText(event.target.value);
+   
+  };
+
+  const [text, setText] = useState("");
+  return (
+    <>
+      <div
+        className="container"
+        style={{ color: props.mode === "dark" ? "white" : "#042743" }}
+      >
+        <div className="mb-3">
+          <h2>{props.heading}</h2>
+          <div className="container my-3">
+            <h5>Your Text Summery</h5>
+            <p>
+              {text.split(" ").filter((element)=>{return element.length!==0}).length} words and {text.length} characters
+            </p>
+          </div>
+          <textarea
+            className="form-control"
+            value={text}
+            onChange={handleOnChange}
+            style={{
+              backgroundColor: props.mode === "dark" ? "#042743" : "white",
+              color: props.mode === "dark" ? "white" : "#042743"
+            }}
+            id="textBox"
+            rows="8"
+          ></textarea>
+        </div>
+        <button className="btn btn-primary mx-2 my-1" onClick={handleUpClick}>
+          Convert To UpperCase
+        </button>
+        <button className="btn btn-primary mx-2 my-1" onClick={handleLoClick}>
+          Convert To LowerCase
+        </button>
+        <button className="btn btn-primary mx-2 my-1" onClick={handleClearClick}>
+          Clear Text
+        </button>
+        <button className="btn btn-primary mx-2 my-1" onClick={handleCopyClick}>
+          Copy Text
+        </button>
+        <button
+          className="btn btn-primary mx-2"
+          onClick={handleExtraSpacesClick}
+        >
+          Remove Extra Spaces
+        </button>
+      </div>
+      <div
+        className="container my-3"
+        style={{ color: props.mode === "dark" ? "white" : "#042743" }}
+      >
+        <p>
+          <b>{0.008 * text.split(" ").filter((element)=>{return element.length!==0}).length} Minutes to read</b>
+        </p>
+        <h2>Preview</h2>
+      <p>{text.length>0?text:"Enter Something it to Preview"}</p>  
+      </div>
+    </>
+  );
+}
